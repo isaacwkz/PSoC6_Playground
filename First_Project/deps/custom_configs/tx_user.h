55b6fb8af07f3e0f53168f0ab021541d55d084c9
Isaac Wong <isaacwongkz@gmail.com>
2024-10-09 03:50:56 +0800

    fix: fix ThreadX startup script and rewrite as a .c source

--- First_Project/deps/custom_configs/tx_user.h
+++ First_Project/deps/custom_configs/tx_user.h
@@ -1,13 +1,12 @@
-/**************************************************************************/
-/*                                                                        */
-/*       Copyright (c) Microsoft Corporation. All rights reserved.        */
-/*                                                                        */
-/*       This software is licensed under the Microsoft Software License   */
-/*       Terms for Microsoft Azure RTOS. Full text of the license can be  */
-/*       found in the LICENSE file at https://aka.ms/AzureRTOS_EULA       */
-/*       and in the root directory of this software.                      */
-/*                                                                        */
-/**************************************************************************/
+/***************************************************************************
+ * Copyright (c) 2024 Microsoft Corporation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License which is available at
+ * https://opensource.org/licenses/MIT.
+ *
+ * SPDX-License-Identifier: MIT
+ **************************************************************************/
 
 /**************************************************************************/
 /**************************************************************************/
@@ -70,38 +69,108 @@
 #ifndef TX_USER_H
 #define TX_USER_H
 
-#ifdef RTOS_TRACE
-#define TX_TRACE_TIME_SOURCE ++_tx_trace_simulated_time
-// #define TX_TRACE_TIME_SOURCE ((unsigned long)0x13000004)
-#define TX_TRACE_TIME_MASK 0xFFFFFFFFUL
-#define TX_ENABLE_EVENT_TRACE
-#endif
-
 #define TX_TIMER_TICKS_PER_SECOND (1000UL)
-
-#define TX_MAX_PRIORITIES 12
-#define TX_TIMER_PROCESS_IN_ISR
+#define TX_MAX_PRIORITIES         32
+#define TX_MINIMUM_STACK          128
+// #define TX_DISABLE_PREEMPTION_THRESHOLD
+// #define TX_DISABLE_REDUNDANT_CLEARING
+// #define TX_DISABLE_NOTIFY_CALLBACKS
+// #define TX_NOT_INTERRUPTABLE
+// #define TX_TIMER_PROCESS_IN_ISR
 #define TX_REACTIVATE_INLINE
 #define TX_DISABLE_STACK_FILLING
 #define TX_ENABLE_STACK_CHECKING
 #define TX_INLINE_THREAD_RESUME_SUSPEND
-// #define TX_BLOCK_POOL_ENABLE_PERFORMANCE_INFO
-// #define TX_BYTE_POOL_ENABLE_PERFORMANCE_INFO
-// #define TX_EVENT_FLAGS_ENABLE_PERFORMANCE_INFO
-// #define TX_MUTEX_ENABLE_PERFORMANCE_INFO
-// #define TX_QUEUE_ENABLE_PERFORMANCE_INFO
-// #define TX_SEMAPHORE_ENABLE_PERFORMANCE_INFO
-// #define TX_THREAD_ENABLE_PERFORMANCE_INFO
-// #define TX_TIMER_ENABLE_PERFORMANCE_INFO
 
-// #define TX_ENABLE_RANDOM_NUMBER_STACK_FILLING
-// #define TX_DISABLE_PREEMPTION_THRESHOLD
-// #define TX_DISABLE_REDUNDANT_CLEARING
-// #define TX_NO_TIMER
-// #define TX_DISABLE_NOTIFY_CALLBACKS
-// #define TX_NO_FILEX_POINTER
-// #define TX_NOT_INTERRUPTABLE
-// #define TX_BYTE_POOL_MULTIPLE_BLOCK_SEARCH    20
-// #define TX_BYTE_POOL_DELAY_VALUE              3
+// #define __NVIC_PRIO_BITS 3
+// #define TX_PORT_USE_BASEPRI
+// #define TX_PORT_BASEPRI (14 << (8 - __NVIC_PRIO_BITS))
+// #define TX_PORT_USE_BASEPRI
+
+/* Determine whether or not stack checking is enabled. By default, ThreadX stack checking is
+   disabled. When the following is defined, ThreadX thread stack checking is enabled.  If stack
+   checking is enabled (TX_ENABLE_STACK_CHECKING is defined), the TX_DISABLE_STACK_FILLING
+   define is negated, thereby forcing the stack fill which is necessary for the stack checking
+   logic.  */
+
+/*
+#define TX_ENABLE_STACK_CHECKING
+*/
+
+/* Determine if the trace event logging code should be enabled. This causes slight increases in
+   code size and overhead, but provides the ability to generate system trace information which
+   is available for viewing in TraceX.  */
+
+/*
+#define TX_ENABLE_EVENT_TRACE
+*/
+
+/* Determine if block pool performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various block pool performance information. */
+
+/*
+#define TX_BLOCK_POOL_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if byte pool performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various byte pool performance information. */
+
+/*
+#define TX_BYTE_POOL_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if event flags performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various event flags performance information. */
+
+/*
+#define TX_EVENT_FLAGS_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if mutex performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various mutex performance information. */
+
+/*
+#define TX_MUTEX_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if queue performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various queue performance information. */
+
+/*
+#define TX_QUEUE_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if semaphore performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various semaphore performance information. */
+
+/*
+#define TX_SEMAPHORE_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if thread performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various thread performance information. */
+
+/*
+#define TX_THREAD_ENABLE_PERFORMANCE_INFO
+*/
+
+/* Determine if timer performance gathering is required by the application. When the following is
+   defined, ThreadX gathers various timer performance information. */
+
+/*
+#define TX_TIMER_ENABLE_PERFORMANCE_INFO
+*/
+
+/*  Override options for byte pool searches of multiple blocks. */
+
+/*
+#define TX_BYTE_POOL_MULTIPLE_BLOCK_SEARCH    20
+*/
+
+/*  Override options for byte pool search delay to avoid thrashing. */
+
+/*
+#define TX_BYTE_POOL_DELAY_VALUE              3
+*/
 
 #endif
